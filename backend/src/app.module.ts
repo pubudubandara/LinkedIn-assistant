13b3518ef2aec0e1fc7af7a14f4beefc0b13eca4
pubudubandara <pubudumb@gmail.com>
2026-01-27 21:24:17 +0530

    Add PostsModule to AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
